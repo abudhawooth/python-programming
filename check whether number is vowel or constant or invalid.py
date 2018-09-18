481e930e833af1a38a8a1c70f540b1886cea210f
abudhawooth <43335492+abudhawooth@users.noreply.github.com>
2018-09-18 15:45:31 +0530

    Create check whether number is vowel or constant or invalid.py

---
+++ check whether number is vowel or constant or invalid.py
@@ -0,0 +1,7 @@
+num=char(input("enter yourletter")
+if (num=a,e,i,o,u):
+     print("your input is vowel")
+elif(num=char):
+     print("your input is not a vowel")
+ else:
+     print("your input is number")
